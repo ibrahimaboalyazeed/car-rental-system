fff0450dec55524a37c4e0859164902eda6fa246
Ibrahim <ibrahimaboalyazeed55@gmail.com>
2023-11-12 22:44:40 +0200

    first commit

--- src/main/java/com/global/CarRentalSystemApplication.java
+++ src/main/java/com/global/CarRentalSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityDemoApplication {
+public class CarRentalSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityDemoApplication.class, args);
+		SpringApplication.run(CarRentalSystemApplication.class, args);
 	}
 
 }
